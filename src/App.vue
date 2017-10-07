e99992173e602833e166557a5650f96a335a3796
adizdar <ahmed.dizdar@viison.com>
2017-10-07 23:04:41 +0200

    Add css font optimization

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,11 @@ export default {
 @import 'assets/style/github-markdown.css';
 
 #app {
+  /* Font optimization */
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  -webkit-text-stroke: 0.45px rgba(0, 0, 0, 0.1);
+  text-shadow: #fff 0px 1px 1px;
 }
 </style>
