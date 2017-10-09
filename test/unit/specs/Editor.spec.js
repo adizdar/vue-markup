6c85dbea6952f98f864688f512ec7c7822e20277
adizdar <ahmed.dizdar@viison.com>
2017-10-09 19:14:18 +0200

    [Unit Test] Add `Editor.spec.js` file

---
+++ test/unit/specs/Editor.spec.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import Editor from '@/views/Editor'
+
+describe('Editor.vue', () => {
+  let vm
+
+  beforeEach(() => {
+    const Constructor = Vue.extend(Editor)
+
+    vm = new Constructor().$mount()
+  })
+
+  it('has default data', () => {
+    expect(Editor.data).to.exist
+    expect(Editor.data).to.be.a('function')
+  })
+
+  it('has the correct default data', () => {
+    const defaultData = Editor.data()
+    expect(defaultData.fromMarkupEditor).to.exist
+  })
+
+  it('sets the correct default data', () => {
+    const defaultData = Editor.data()
+    expect(defaultData.fromMarkupEditor).to.be.empty
+  })
+
+  it('should have computed', () => {
+    expect(Editor.computed).to.exist
+  })
+
+  it('has convertMarkupEditorData in computed', () => {
+    expect(Editor.computed.convertMarkupEditorData).to.exist
+    expect(Editor.computed.convertMarkupEditorData).to.be.a('function')
+  })
+
+  it('sets the computed convertMarkupEditorData after data update', () => {
+    vm.fromMarkupEditor = '**Foo**'
+    expect(vm.convertMarkupEditorData).to.not.be.empty
+  })
+
+  it('updates the rendered message when vm.fromMarkupEditor updates', done => {
+    vm.fromMarkupEditor = 'foo'
+
+    // wait a "tick" after state change before asserting DOM updates
+    Vue.nextTick(() => {
+      expect(vm.$el.textContent).to.not.be.empty
+      done()
+    })
+  })
+})
