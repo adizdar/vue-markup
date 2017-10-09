403c331f796e185e4adb9cb121b793626dbc071d
adizdar <ahmed.dizdar@viison.com>
2017-10-09 10:26:10 +0200

    [Unit Test] Add `MarkupMirror.spec.vue` file

---
+++ test/unit/specs/MarkupEditor.spec.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import MarkupEditor from '@/components/MarkupEditor/MarkupEditor'
+
+describe('MarkupEditor.vue', () => {
+  let clock
+
+  beforeEach(() => {
+    clock = sinon.useFakeTimers()
+  })
+
+  afterEach(() => {
+    clock.restore()
+  })
+
+  it('has methods defined', () => {
+    expect(MarkupEditor.methods).to.exist
+  })
+
+  it('has $_commitMarkup method defined', () => {
+    expect(MarkupEditor.methods.$_commitMarkup).to.exist
+  })
+
+  it('should emit an input event with $_commitMarkup after 400 ticks', () => {
+    const Constructor = Vue.extend(MarkupEditor)
+    const vm = new Constructor()
+
+    // Wrap vm's method $emit to record/spy on it
+    sinon.spy(vm, '$emit')
+
+    // Call method to trigger the built in emit
+    vm.$_commitMarkup('20')
+
+    // Set fake timmer to tick 400 ms so loadash can call the input event
+    clock.tick(400)
+
+    // Get the recording of the spy object
+    expect(vm.$emit).to.have.been.calledWith('input')
+  })
+})
