8eea4ac0deabd54224dc9ea357e3376e7bfb9420
adizdar <ahmed.dizdar@viison.com>
2017-10-14 23:03:01 +0200

    Update font family priority load

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
 
 #app {
   /* Font optimization */
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, 'Roboto', Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   -webkit-text-stroke: 0.45px rgba(0, 0, 0, 0.1);
