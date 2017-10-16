a98fe9f99e511a147bd84c4648d465fe9576d641
adizdar <ahmed.dizdar@viison.com>
2017-10-17 00:47:45 +0200

    Add `DropdownSettings` component
    Temporally UI component for the rest of the features

---
+++ src/components/DropdownSettings/DropdownSettings.vue
@@ -0,0 +1,46 @@
+<template lang="html">
+  <div class="vmd-dropdown--container">
+    <!-- Dropdown Trigger -->
+    <a
+      class='dropdown-button btn'
+      href='#'
+      data-activates='dropdown_settings'>
+      <i class="material-icons large">more_vert</i>
+    </a>
+
+    <!-- Dropdown Structure -->
+    <ul id='dropdown_settings' class='dropdown-content'>
+      <li><a href="#!">one</a></li>
+      <li><a href="#!">two</a></li>
+      <li class="divider"></li>
+      <li><a href="#!">three</a></li>
+      <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
+      <li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'dropdownSettings'
+}
+</script>
+
+<style lang="css" scoped>
+  .vmd-dropdown--container .btn {
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+    background-color: transparent;
+    color: black;
+    padding: 0.4rem 0.8rem;
+  }
+
+  .vmd-dropdown--container .btn i {
+    font-size: 1.8rem;
+  }
+
+  .vmd-dropdown--container .dropdown-content > li > a {
+    color: #37474f;
+  }
+</style>
