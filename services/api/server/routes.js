919ed895ebd0c2ac6120ae158add11679da62d97
adizdar <ahmed.dizdar@viison.com>
2017-12-03 14:44:47 +0100

    [Api] Include save content controller

--- services/api/server/routes.js
+++ services/api/server/routes.js
@@ -1,8 +1,10 @@
-import htmlToPdfRouter from './api/controllers/downloadContent/router';
+import downloadContent from './api/controllers/downloadContent/router';
+import saveContent from './api/controllers/saveContent/router';
 
 const URL_PATH = '/api';
 const VERSION_ONE = '/v1';
 
 export default function routes(app) {
-  app.use(`${URL_PATH + VERSION_ONE}/download`, htmlToPdfRouter);
+  app.use(`${URL_PATH + VERSION_ONE}/download`, downloadContent);
+  app.use(`${URL_PATH + VERSION_ONE}/save`, saveContent);
 }
